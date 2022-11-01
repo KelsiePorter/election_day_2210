0582e511443bec519075f908c0ed6150b6d5dd1a
Kelsie Porter <krporter22@gmail.com>
2022-11-01 09:48:38 -0600

    Create #register_candidate! tests and associated method

--- lib/race.rb
+++ lib/race.rb
@@ -1,9 +1,19 @@
+require 'pry'
+require './lib/candidate'
+
 class Race
   attr_reader :office,
               :candidates
 
   def initialize(office)
     @office = office
+    @candidates = []
+  end
+
+  def register_candidate!(candidate_info)
+    new_candidate = Candidate.new(candidate_info)
+    @candidates << new_candidate
+    new_candidate
   end
 
 end
--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -9,4 +9,19 @@ RSpec.describe Race do
     expect(race.office).to eq("Texas Governor")
   end
 
+  it 'each race has candidiates running' do 
+    expect(race.candidates).to eq([])
+
+    candidate1 = race.register_candidate!({name: "Diana D", party: :democrat})
+
+    expect(candidate1).to be_a Candidate
+    expect(candidate1.name).to eq("Diana D")
+    expect(candidate1.party).to eq(:democrat)
+    expect(race.candidates).to eq([candidate1])
+
+    candidate2 = race.register_candidate!({name: "Roberto R", party: :republican})
+    
+    expect(race.candidates).to eq([candidate1, candidate2])
+  end
+
 end
