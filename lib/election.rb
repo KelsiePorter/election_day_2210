00ce39ad30b834fcaa7520cb6394c2a9fd67ba1d
Kelsie Porter <krporter22@gmail.com>
2022-11-01 10:12:11 -0600

    Create #candidates and associated tests

--- lib/election.rb
+++ lib/election.rb
@@ -4,6 +4,19 @@ class Election
 
   def initialize(year)
     @year = year 
+    @races = []
   end
-  
+
+  def add_race(race)
+    @races << race
+  end
+
+  def candidates 
+    candidates = []
+    @races.select do |race|
+      candidates << race.candidates
+    end
+    candidates.flatten! 
+  end
+
 end
--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -21,4 +21,40 @@ RSpec.describe Election do
 
     expect(election.races).to eq([race1, race2])
   end
+
+  it 'An election has multiple candidates' do 
+    race1 = Race.new("Virginia District 4 Representative")
+    race2 = Race.new("Texas Governor")
+
+    election.add_race(race1)
+    election.add_race(race2)
+
+    expect(election.candidates).to eq([])
+
+    candidate1 = race1.register_candidate!({name: "Diana D", party: :democrat})
+    candidate2 = race1.register_candidate!({name: "Roberto R", party: :republican})
+    candidate3 = race2.register_candidate!({name: "Diego D", party: :democrat})
+    candidate4 = race2.register_candidate!({name: "Rita R", party: :republican})
+    candidate5 = race2.register_candidate!({name: "Ida I", party: :independent})
+
+    expect(election.candidates).to eq([candidate1, candidate2, candidate3, candidate4, candidate5])
+  end
+
+  it 'An election counts the number of votes received' do 
+    race1 = Race.new("Virginia District 4 Representative")
+    race2 = Race.new("Texas Governor")
+    candidate1 = race1.register_candidate!({name: "Diana D", party: :democrat})
+    candidate2 = race1.register_candidate!({name: "Roberto R", party: :republican})
+    candidate3 = race2.register_candidate!({name: "Diego D", party: :democrat})
+    candidate4 = race2.register_candidate!({name: "Rita R", party: :republican})
+    candidate5 = race2.register_candidate!({name: "Ida I", party: :independent})
+
+    election.add_race(race1)
+    election.add_race(race2)
+
+    4.times {candidate1.vote_for!}
+
+
+  end
+
 end
