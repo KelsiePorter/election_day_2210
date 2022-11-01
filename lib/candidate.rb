0747fceeacd5b485cfd91c44b052e9f437ca16dc
Kelsie Porter <krporter22@gmail.com>
2022-11-01 09:12:00 -0600

    Create Candidate class

--- lib/candidate.rb
+++ lib/candidate.rb
@@ -1,2 +1,10 @@
 class Candidate
+  attr_reader :name,
+              :party 
+
+  def initialize(candinate_info)
+    @name = candinate_info[:name]
+    @party = candinate_info[:party]
+  end
+
 end
